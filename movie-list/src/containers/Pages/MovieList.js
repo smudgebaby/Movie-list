c41f7a7d7c3649f168aa70b233af8eadd5af6b5f
liuxinpu16 <liuxinpu16@gmail.com>
2022-05-06 16:07:50 -0500

    bug-fix-and-movielist-logic

--- movie-list/src/containers/Pages/MovieList.js
+++ movie-list/src/containers/Pages/MovieList.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { useState } from "react";
 
 const MovieList = () => {
-  return <div>MovieList</div>;
+  const [curpage, setCurpage] = useState(1);
+  const [movies, setmovies] = useState([]);
+/* 
+  function likeMovie(id) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function blockMovie(id) {
+    setmovies(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+ */
+
+
+
+  return (
+    <div>
+      <div>
+        <button>-</button>
+        <p>{curpage}/1000 page</p>
+        <button>+</button>
+      </div>
+      <div>
+        {/* {movies.map((movie) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={noteItem.title}
+              content={noteItem.content}
+              onDelete={deleteNote}
+            />
+          );
+        })}; */}
+      </div>
+    </div>
+  );
 };
 
 export default MovieList;
