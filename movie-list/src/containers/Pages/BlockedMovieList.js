c1d07a32ac9b15c36848b4bb436fd832368840b2
Xinyi Zhao <zxy1307@bu.edu>
2022-05-08 01:33:13 -0400

    part of like/block list logic

--- movie-list/src/containers/Pages/BlockedMovieList.js
+++ movie-list/src/containers/Pages/BlockedMovieList.js
@@ -1,11 +1,64 @@
 import React from "react";
 import BlockedMovieListCard from '../../Components/Card/BlockedMovieListCard'
+import './BlockedMovieList.css'
 
-const BlockedMovieList = () => {
+export default function BlockedMovieList(){
+  function unblockMovie(){
 
+  }
+
+  function likeMovie(){
+
+  }
+
+
+  const props=[{
+    id:"278",
+    poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    },
+    {id:"211",
+    poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+    },
+    {
+      id:"278",
+      poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+      },
+      {id:"211",
+      poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+      },
+      {
+        id:"278",
+        poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+        },
+        {id:"211",
+        poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+        },
+        {
+          id:"278",
+          poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+          },
+          {id:"211",
+          poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+          }]
+  
 
   
-  return <div>BlockedMovieList</div>;
-};
-
-export default BlockedMovieList;
+  return (
+    <div>
+      <div>BlockedMovieList</div>
+      <div className="container">
+        {props.map(item =>{
+          return (
+            <BlockedMovieListCard 
+            poster_path={item.poster_path}
+            id = {item.id}
+            addUnBlock={unblockMovie}
+            addLike={likeMovie}
+            />
+          );
+        })}
+      </div>
+    </div>
+  
+  )
+}
--- movie-list/src/containers/Pages/LikeMovieList.js
+++ movie-list/src/containers/Pages/LikeMovieList.js
@@ -1,15 +1,69 @@
 import React from "react";
 import LikeMovieListCard from "../../Components/Card/LikeMovieListCard";
+import './LikeMovieList.css'
+import { useSelector, useDispatch } from 'react-redux';
+import {unlike, likedToBlocked} from '../../features/movieList/AllInOneSlice.js'
 
-const LikeMovieList = () => {
+export default function LikeMovieList(){
+  const [likedMovies, setLikedMovies] = React.useState([])
+  const [blokedMovies, setBlockedMovies] = React.useState([])
+  const dispatch = useDispatch();
+  function UnlikeMovie(id){
+    dispatch(unlike(id))
+    setLikedMovies(useSelector((state) => state.movieList.liked))
+  }
+
+  function BlockMovie(){
+
+  }
+
+  const props=[{
+  id:"278",
+  poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  },
+  {id:"211",
+  poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+  },
+  {
+    id:"278",
+    poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    },
+    {id:"211",
+    poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+    },
+    {
+      id:"278",
+      poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+      },
+      {id:"211",
+      poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+      },
+      {
+        id:"278",
+        poster_path:"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+        },
+        {id:"211",
+        poster_path:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg"
+        }]
+
   return (
     <div>
       <div>LikeMovieList</div>
-      {/* temp setting, will change by map */}
-      <LikeMovieListCard />
+      <div className="container">
+        {props.map(item =>{
+          return (
+            <LikeMovieListCard 
+            poster_path={item.poster_path}
+            id = {item.id}
+            addUnlike={UnlikeMovie}
+            addBlock={BlockMovie}
+            />
+          );
+        })}
+      </div>
     </div>
   
-  );
-};
+  )
+}
 
-export default LikeMovieList;
+
