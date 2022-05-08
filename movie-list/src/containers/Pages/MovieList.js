df8932820e83a26b60dbfab65dd3fd8de7731e78
Lyon <t41589@gmail.com>
2022-05-08 05:19:42 -0400

    pull the Card Component

--- movie-list/src/containers/Pages/MovieList.js
+++ movie-list/src/containers/Pages/MovieList.js
@@ -4,26 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./MovieList.css";
 
 const MovieList = () => {
-  const IMG_URL = "https://image.tmdb.org/t/p/w500";
-  const [movies, setMovies] = useState([]);
-
-  const getMovieRequest = async () => {
-    const url =
-      "https://api.themoviedb.org/3/movie/top_rated?api_key=29690f179ec220f023f10bc594c0637e";
-    try {
-      const response = await fetch(url);
-      const responseJson = await response.json();
-      console.log(responseJson);
-      setMovies(responseJson.results);
-    } catch (err) {
-      console.log("catch err this is wrong", err);
-    }
-  };
-  useEffect(() => {
-    getMovieRequest();
-  }, []);
-  console.log(movies);
-
   return (
     <div className={"container-fluid d-flex flex-wrap justify-content-center "}>
       {movies.map((movie) => {
@@ -43,3 +23,4 @@ const MovieList = () => {
 };
 
 export default MovieList;
+Re-pull the Card Component
