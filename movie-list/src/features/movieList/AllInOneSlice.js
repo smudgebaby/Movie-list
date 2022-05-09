62fce99e12cb697031e8b6f106b6152a233bedad
Lyon <t41589@gmail.com>
2022-05-08 23:41:08 -0400

    re pull again

--- movie-list/src/features/movieList/AllInOneSlice.js
+++ movie-list/src/features/movieList/AllInOneSlice.js
@@ -1,56 +1,63 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const AllinOneSlice = createSlice({
-  name: 'movieList',
+  name: "movieList",
   initialState: {
-    liked: Set(),
-    blocked: Set(),
-    pageIds: {},
-    movieInfo:{},
+    liked: [],
+    blocked: [],
+    pageIds: [],
+    movieInfo: [],
   },
   reducers: {
-    like:(state,action)=> {
-        state.liked.add(action.payload)
-        return state
+    like: (state, action) => {
+      state.liked.add(action.payload);
+      return state;
     },
 
-    block:(state,action)=> {
-        state.blocked.add(action.payload)
-        return state
+    block: (state, action) => {
+      state.blocked.add(action.payload);
+      return state;
     },
 
-    unlike:(state,action)=>  {
-        state.liked.delete(action.payload)
-        return state
+    unlike: (state, action) => {
+      state.liked.delete(action.payload);
+      return state;
     },
 
-    unblock:(state,action)=> {
-        state.blocked.delete(action.payload)
-        return state
+    unblock: (state, action) => {
+      state.blocked.delete(action.payload);
+      return state;
     },
 
-    likedToBlocked:(state,action)=> {
-        state.liked.delete(action.payload)
-        state.blocked.add(action.payload)
-        return state
+    likedToBlocked: (state, action) => {
+      state.liked.delete(action.payload);
+      state.blocked.add(action.payload);
+      return state;
     },
 
-    blockedToLiked:(state,action)=> {
-        state.blocked.delete(action.payload)
-        state.liked.add(action.payload)
-        return state
+    blockedToLiked: (state, action) => {
+      state.blocked.delete(action.payload);
+      state.liked.add(action.payload);
+      return state;
     },
 
-    update:(state,action)=> {
-        state.pageIds[action.payload.pageNumber] = action.payload.curPageIds
-        action.payload.movieInfos.forEach(info => {
-            state.movieInfo[info.id] = info
-        });
-        return state
+    update: (state, action) => {
+      state.pageIds[action.payload.pageNumber] = action.payload.curPageIds;
+      action.payload.movieInfos.forEach((info) => {
+        state.movieInfo[info.id] = info;
+      });
+      return state;
     },
-  }
-})
+  },
+});
 
-
-export const { like,block,unlike,unblock,likedToBlocked,blockedToLiked,update } = AllinOneSlice.actions
-export default AllinOneSlice.reducer
+export const {
+  like,
+  block,
+  unlike,
+  unblock,
+  likedToBlocked,
+  blockedToLiked,
+  update,
+} = AllinOneSlice.actions;
+export default AllinOneSlice.reducer;
