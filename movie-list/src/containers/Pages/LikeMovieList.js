dcea55ea7adef34b76bca427ce3cf73f663a91c3
Xinyi Zhao <zxy1307@bu.edu>
2022-05-09 19:04:02 -0400

    like page and button logic

--- movie-list/src/containers/Pages/LikeMovieList.js
+++ movie-list/src/containers/Pages/LikeMovieList.js
@@ -1,15 +1,47 @@
 import React from "react";
 import LikeMovieListCard from "../../Components/Card/LikeMovieListCard";
+import './LikeMovieList.css'
+import { useSelector, useDispatch } from 'react-redux';
+import {unlike, likedToBlocked} from '../../features/movieList/AllInOneSlice.js'
 
-const LikeMovieList = () => {
+export default function LikeMovieList(){
+  
+  const dispatch = useDispatch();
+  const movieInfo = useSelector((state)=> state.movieList.movieInfo)
+
+  var likedIds = useSelector((state) => state.movieList.liked)
+  var likedInfo = movieInfo.filter(m => likedIds.includes(m.id)) 
+  const [likedMovies, setLikedMovies] = React.useState(likedInfo)
+
+  function UnlikeMovie(id){
+    dispatch(unlike(id))
+    setLikedMovies(prevMovies =>{return prevMovies.filter(m=>m.id !==id)})
+  }
+
+  function BlockMovie(id){
+    dispatch(likedToBlocked(id))
+    setLikedMovies(prevMovies =>{return prevMovies.filter(m=>m.id !==id)})
+  }
+
+  //console.log(movieInfo)
+  //console.log(likedMovies)
+
   return (
     <div>
       <div>LikeMovieList</div>
-      {/* temp setting, will change by map */}
-      <LikeMovieListCard />
+      <div className="container">
+        {likedMovies.map(item =>{
+          return (
+            <LikeMovieListCard 
+            poster_path={item.poster_path}
+            id = {item.id}
+            addUnlike={UnlikeMovie}
+            addBlock={BlockMovie}
+            />
+          );
+        })}
+      </div>
     </div>
   
-  );
-};
-
-export default LikeMovieList;
+  )
+}
